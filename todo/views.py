8349bde9c4e9184357b7dc4fced99555bee25934
mohammadreza <mohammadreza13akbari88@gmail.com>
2026-07-15 11:04:21 +0330

    add post method to Serializer

--- todo/views.py
+++ todo/views.py
@@ -9,8 +9,15 @@ from .serializes import todoserializer
 # Create your views here.
 
 
-@api_view(["GET"])
+@api_view(["GET","POST"])
 def all_todos(request: Request):
-    todo = list(todoModel.objects.order_by("privory").all())
-    todoSerializer = todoserializer(todo, many=True)
-    return Response(todoSerializer.data, status.HTTP_200_OK)
+    if request.method == "GET":
+        todo = list(todoModel.objects.order_by("privory").all())
+        todoSerializer = todoserializer(todo, many=True)
+        return Response(todoSerializer.data, status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = todoserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+    return Response(serializer.data, status.HTTP_400_BAD_REQUEST)

1254ca5f50b8ec9dce38d1909220f6affe33abc1
mohammadreza <mohammadreza13akbari88@gmail.com>
2026-07-15 21:01:10 +0330

    added todo_details_view to project

--- todo/urls.py
+++ todo/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
-from .views import all_todos
+from .views import all_todos, todo_details_view
 
-urlpatterns = [path("", all_todos)]
+urlpatterns = [path("", all_todos), path("<int:todo_id>", todo_details_view)]
--- todo/views.py
+++ todo/views.py
@@ -9,7 +9,7 @@ from .serializes import todoserializer
 # Create your views here.
 
 
-@api_view(["GET","POST"])
+@api_view(["GET", "POST"])
 def all_todos(request: Request):
     if request.method == "GET":
         todo = list(todoModel.objects.order_by("privory").all())
@@ -21,3 +21,24 @@ def all_todos(request: Request):
             serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
     return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def todo_details_view(request: Request, todo_id: int):
+    try:
+        todo = todoModel.objects.get(pk=todo_id)
+    except todoModel.DoesNotExist:
+        return Response(None, status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = todoserializer(todo)
+        return Response(serializer.data, status.HTTP_200_OK)
+    elif request.method == "PUT":
+        serializer = todoserializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_202_ACCEPTED)
+        return Response(None, status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        todo.delete()
+        return Response(None, status.HTTP_200_OK)

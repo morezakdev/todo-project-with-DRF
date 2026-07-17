d51897df344cb5bd1d18fe01425530a563dd27e0
mohammadreza <mohammadreza13akbari88@gmail.com>
2026-07-18 00:56:31 +0330

    added the Swagger UI to project and end first project with Django Rest Framework

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "base",
     "todo",
     "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"}
